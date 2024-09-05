6414f07a41665f1ffa549bfd05ac8d5984f22962
VF <vasul.fr@gmail.com>
2024-09-05 18:47:44 +0300

    loops

--- index.js
+++ index.js
@@ -5,38 +5,20 @@ const path = require('node:path');
 
 const app = async () => {
 
-   await fs.mkdir(path.join(__dirname, 'baseFolder', 'folder1'), {recursive: true})
-   await fs.mkdir(path.join(__dirname, 'baseFolder', 'folder2'), {recursive: true})
-   await fs.mkdir(path.join(__dirname, 'baseFolder', 'folder3'), {recursive: true})
-   await fs.mkdir(path.join(__dirname, 'baseFolder', 'folder4'), {recursive: true})
-   await fs.mkdir(path.join(__dirname, 'baseFolder', 'folder5'), {recursive: true})
+   const folders = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5'];
 
-   await fs.writeFile(path.join(__dirname, 'baseFolder', 'folder1', 'text1.txt'), 'Hellow 1');
-   const stat1 = await fs.stat(path.join(__dirname, 'baseFolder', 'folder1', 'text1.txt'))
-   console.log(path.resolve( 'baseFolder', 'folder1', 'text1.txt'))
-   console.log('file:', stat1.isFile(), 'folder:',stat1.isDirectory())
+   await fs.mkdir(path.join(__dirname, 'baseFolder'));
 
-   await fs.writeFile(path.join(__dirname, 'baseFolder', 'folder2', 'text2.txt'), 'Hellow 2');
-   const stat2 = await fs.stat(path.join(__dirname, 'baseFolder', 'folder2', 'text2.txt'))
-   console.log(path.resolve( 'baseFolder', 'folder2', 'text2.txt'))
-   console.log('file:', stat2.isFile(), 'folder:',stat2.isDirectory())
+   const pathBase = path.join(__dirname, 'baseFolder');
 
-   await fs.writeFile(path.join(__dirname, 'baseFolder', 'folder3', 'text3.txt'), 'Hellow 3');
-   const stat3 = await fs.stat(path.join(__dirname, 'baseFolder', 'folder3', 'text3.txt'))
-   console.log(path.resolve( 'baseFolder', 'folder3', 'text3.txt'))
-   console.log('file:', stat3.isFile(), 'folder:',stat3.isDirectory())
+   folders.forEach( async (folder) => {
+      await fs.mkdir(path.join(pathBase, `${folder}`), {recursive: true});
+      await fs.writeFile(path.join(pathBase, `${folder}`, 'text.txt'), 'Hello');
 
-   await fs.writeFile(path.join(__dirname, 'baseFolder', 'folder4', 'text4.txt'), 'Hellow 4');
-   const stat4 = await fs.stat(path.join(__dirname, 'baseFolder', 'folder4', 'text4.txt'))
-   console.log(path.resolve( 'baseFolder', 'folder4', 'text4.txt'))
-   console.log('file:', stat4.isFile(), 'folder:',stat4.isDirectory())
-
-   await fs.writeFile(path.join(__dirname, 'baseFolder', 'folder5', 'text5.txt'), 'Hellow 5');
-   const stat5 = await fs.stat(path.join(__dirname, 'baseFolder', 'folder5', 'text5.txt'))
-   console.log(path.resolve( 'baseFolder', 'folder5', 'text5.txt'))
-   console.log('file:', stat5.isFile(), 'folder:',stat5.isDirectory())
-
-   
+      const stat = await fs.stat(path.join(pathBase, `${folder}`, 'text.txt'))
+      console.log(path.resolve( 'baseFolder', `${folder}`, 'text.txt'))
+      console.log('file:', stat.isFile(), 'folder:',stat.isDirectory())
+   })
    
 }
 
